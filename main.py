c007c0ee83c58bed971b26990ebb3922f237612d
almaashi2026-a11y <almaashi2026@gmail.com>
2026-06-28 19:25:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import threading
 from flask import Flask
 
-from scanner import get_stock_price
+from scanner import scan_market
 from telegram_bot import send_message
 
 # -----------------------------
@@ -12,33 +12,39 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "NASDAQ Scanner is running!"
+    return "NASDAQ Scanner Running"
 
 def run_web_server():
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
 # -----------------------------
-# تشغيل البوت
+# تشغيل السكنر
 # -----------------------------
 def run_bot():
 
     print("🚀 NASDAQ Scanner Started")
 
-    stock = get_stock_price("NVDA")
+    send_message("✅ بدأ فحص سوق NASDAQ")
 
-    if stock:
+    results = scan_market()
 
-        message = f"""✅ Finnhub متصل
+    if not results:
+        send_message("❌ لم يتم العثور على بيانات.")
+        return
 
-السهم: {stock['symbol']}
-السعر: ${stock['price']}
-أعلى سعر: ${stock['high']}
-أقل سعر: ${stock['low']}
-"""
+    message = "📊 أفضل 5 أسهم حالياً\n\n"
 
-        print(message)
-        send_message(message)
+    for stock in results[:5]:
+
+        message += (
+            f"📈 {stock['symbol']}\n"
+            f"⭐ Score: {stock['score']}/100\n"
+            f"💲 السعر: ${stock['price']}\n"
+            f"📊 التغير: {stock['change_percent']:.2f}%\n\n"
+        )
+
+    send_message(message)
 
 # -----------------------------
 # Main
